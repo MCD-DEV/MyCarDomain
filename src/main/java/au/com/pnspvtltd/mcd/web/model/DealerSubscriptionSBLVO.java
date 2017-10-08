bfaf9be86f6c1871fa4f7b8b964f932aa6fe7b2a
pavan kumar <pavan.upperpelly@gmail.com>
2017-10-08 15:49:20 +0530

    sticky

---
+++ src/main/java/au/com/pnspvtltd/mcd/web/model/DealerSubscriptionSBLVO.java
@@ -0,0 +1,27 @@
+package au.com.pnspvtltd.mcd.web.model;
+
+import java.util.List;
+
+public class DealerSubscriptionSBLVO {
+
+	
+	public Long getDealerId() {
+		return dealerId;
+	}
+
+	public void setDealerId(Long dealerId) {
+		this.dealerId = dealerId;
+	}
+
+	public List<VehicleDealerDetailsVO> getVehicleDealerDetails() {
+		return vehicleDealerDetails;
+	}
+
+	public void setVehicleDealerDetails(List<VehicleDealerDetailsVO> vehicleDealerDetails) {
+		this.vehicleDealerDetails = vehicleDealerDetails;
+	}
+
+	private Long dealerId;
+	
+	List<VehicleDealerDetailsVO> vehicleDealerDetails;
+}
